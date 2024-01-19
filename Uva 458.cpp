87b01d49d76abda896d8b7ddcdb83cb0730af0d6
shamiha16 <155057582+shamiha16@users.noreply.github.com>
2024-01-19 20:42:01 +0600

    Add files via upload

---
+++ Uva 458.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    while(scanf("%c",&c)!=EOF)
+    {
+        if (c!='\n')
+            printf("%c",c-7);
+        else
+            printf("\n");
+    }
+    return 0;
+}
